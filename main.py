0924d1e4128efa064963ee40bd7046b220dc6492
semihcngz <scengiz2002@hotmail.com>
2025-10-02 19:15:24 +0300

    first response

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
-print(dotenv_values(".env").get('OPENAI_API_KEY'))
+model = ChatOpenAI(model='gpt-5-nano', temperature=0.1)
+message = [
+    SystemMessage(content='Translate the following from english to turkish'),
+    HumanMessage(content='Hi!')
+]
+
+
+
+
+if __name__ == '__main__':
+    response = model.invoke(message)
+    print(response.content)
+
+
