4d251c230dffe42a6b71dcb03108561d46e6a321
semihcngz <scengiz2002@hotmail.com>
2025-10-02 20:37:55 +0300

    langserve

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-
+from fastapi import FastAPI
+from langserve import add_routes
 
 load_dotenv()
 
@@ -27,8 +28,18 @@ parser = StrOutputParser()
 
 chain = prompt_template | model | parser #modelin ciktisini parsera ver
 
+app = FastAPI(
+    title='LangChain API',
+    version='1.0',
+    description='A simple language chain API',
+)
+
+add_routes(app, chain, path='/chain')
+
+
 
 if __name__ == '__main__':
-    print(chain.invoke({'language': 'spanish', 'text': 'Hello, world!'}))
-
+    #print(chain.invoke({'language': 'spanish', 'text': 'Hello, world!'}))
+    import uvicorn
+    uvicorn.run(app, host='localhost', port=8000)
 
